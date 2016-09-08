c0fded07b632398a38e18f1302a223331a46aaa7
David L. Inman <davidlevere@gmail.com>
2016-09-07 23:27:06 -0400

    inc prog

--- ParserUtil.cs
+++ ParserUtil.cs
@@ -1,9 +1,43 @@
 
 namespace Parsing 
 {
+    public class ParseBuffer
+    {
+
+    }
+
+    public class FailData
+    {
+        
+    }
+
+    public class ParseResult<T>
+    {
+        public readonly bool IsSuccessful;
+        public readonly T Result;
+        public readonly FailData Failure;
+
+        public ParseResult( T result )
+        {
+            IsSuccessful = true;
+            Result = result;
+        }
+        public ParseResult( FailData f )
+        {
+            IsSuccessful = false;
+            Failure = f;
+        }
+    }
+
+    public delegate ParseResult<T> Parser<T>( ParseBuffer buffer );
+
     // TODO need parsing result
     // TODO need parse source
     public static class ParserUtil
     {
+        public static Parser<B> Bind<A, B>( Parser<A> parser, Func<A, Parser<B>> gen )
+        {
+            
+        }
     }
 }
