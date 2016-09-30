8673dba86d65f07695286da5ed081bba92398640
David L. Inman <davidlevere@gmail.com>
2016-09-30 19:12:53 -0400

    boolean parser

--- Parsing/LangParser.cs
+++ Parsing/LangParser.cs
@@ -1,8 +1,14 @@
 
+using Box.AST;
 
 namespace Box.Parsing
 {
     public static class LangParser
     {
+        public static Parser<Boolean> Boolean = 
+            ParserUtil.Alternate( 
+                ParserUtil.Match( "true" ), 
+                ParserUtil.Match( "false" ) )
+            .Map( value => new Boolean( value == "true" ) );
     }
 }
