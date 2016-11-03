2b770dbbe9990b3a3c05e5a2f9659af1fc7947d6
David L. Inman <davidlevere@gmail.com>
2016-11-02 21:35:37 -0400

    inc prog for testing framework

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -1,32 +1,77 @@
 
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+
 
 namespace Box.Tests
 {
     public static class TestRunner
     {
         public static void RunTests<T>()
+            where T : new()
         {
-            var x = typeof( T ).GetMethods( BindingFlags.Public );
-            foreach ( var xlet in x )
+            var results = new List<string>();
+            var target = new T();
+            var methods = typeof( T ).GetMethods( BindingFlags.Public | BindingFlags.Instance );
+            foreach ( var method in methods )
             {
-                foreach( var z in xlet.GetCustomAttributes( typeof( Test ), true ) )
+                if ( method.GetCustomAttributes( typeof( Test ), true ).Length != 0 )
                 {
-                    Console.WriteLine( "Blarg" + z );
+                    var pass = false;
+                    try
+                    {
+                        method.Invoke( target, null );
+                        pass = true;   
+                    }
+                    catch ( TargetInvocationException e )
+                    {
+                        var ae = e.InnerException as AssertException;
+                        if ( ae != null )
+                        {
+                            results.Add( method.Name +  ": Failed: " + ae.Message );
+                        }
+                        else
+                        {
+                            results.Add( method.Name + ": Failed due to unexpected exception: " + e.InnerException );
+                        }
+                    }
+                    if ( pass )
+                    {
+                        results.Add( method.Name + ": pass" );
+                    }
                 }
             }
 
+            // TODO should probably return this
+            foreach( var result in results )
+            {
+                Console.WriteLine( result );
+            }
         }
     }
 
     public static class Assert
-    {
-        
+    {   
+        public static void IsTrue( bool value )
+        {
+            if ( !value )
+            {
+                throw new AssertException("Expected true got false.");
+            }
+        }   
     }
 
     public class Test : Attribute
     {
 
+    }
+
+    public class AssertException : Exception
+    {
+        public AssertException(string message)
+            : base( message )
+        {
+        }
     }
 }
