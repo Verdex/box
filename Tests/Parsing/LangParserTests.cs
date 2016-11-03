600a5fd38068b4c8e208cc77342c6f6510c8df19
David L. Inman <davidlevere@gmail.com>
2016-11-02 21:35:58 -0400

    initial test for lang parser

--- Tests/Parsing/LangParserTests.cs
+++ Tests/Parsing/LangParserTests.cs
@@ -1,14 +1,22 @@
 
 using Box.Tests;
 
+using Box.Parsing;
+
 namespace Box.Tests.Parsing
 {
     public class LangParserTests
     {
         [Test]
-        public void Test()
+        public void BoolParsesTrue()
         {
+            var buffer = new ParseBuffer( @"true", 0 );
+            var ret = LangParser.Boolean( buffer );
 
+            Assert.IsTrue( ret.IsSuccessful );
+        
+            Assert.IsTrue( ret.Result != null ); // TODO replace with not null assert method
+            Assert.IsTrue( ret.Result.Value );
         }
     }
 }
